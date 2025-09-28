be5e3171007ef91cab22bc5d9f2e49e3dec5e681
Evgeny <sjs-master@yandex.ru>
2025-09-29 00:16:22 +0700

    Update routing.ts (#37) (#38)

---
+++ src/i18n/createUrl.ts
@@ -0,0 +1,15 @@
+import type { Locale } from 'next-intl'
+import { getPathname } from '@/i18n/navigation'
+import { URL } from '@/variables/url'
+
+export type Href = Parameters<typeof getPathname>[0]['href']
+
+export interface UrlArgs {
+  locale: Locale
+  href: Href
+}
+
+export const createUrl = ({ locale, href }: UrlArgs): string => {
+  const pathname = getPathname({ locale, href })
+  return `https://${URL}${pathname}`
+}
