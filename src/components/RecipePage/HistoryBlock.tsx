adc7f936fe06843c6eee25f55235bd45b33f0364
AtrVorobev <artvorobev96@gmail.com>
2025-10-12 19:14:18 +0300

    fix

--- src/components/RecipePage/HistoryBlock.tsx
+++ src/components/RecipePage/HistoryBlock.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import styled from 'styled-components'
-import { VariationsList } from '../VariationsList'
 import { maxDevice } from '@/styles/device'
 import { DishHistory } from '../DishHistory'
 
