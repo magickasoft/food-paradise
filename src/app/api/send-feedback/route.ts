c0d1218959d658f24cd67f4d6d633ce5f43c2b8e
AtrVorobev <artvorobev96@gmail.com>
2025-10-15 21:24:53 +0300

    fix

--- src/app/api/send-feedback/route.ts
+++ src/app/api/send-feedback/route.ts
@@ -3,25 +3,25 @@ import nodemailer from 'nodemailer'
 
 export async function POST(req: Request) {
   try {
-    const { rating, comment } = await req.json()
+    const { recipe, rating, comment } = await req.json()
 
     const transporter = nodemailer.createTransport({
-      host: 'smtp.mail.ru',
-      port: 465,
+      ...(process.env.MAIL_HOST && { host: process.env.MAIL_HOST }),
+      port: process.env.MAIL_PORT,
       secure: true,
       auth: {
-        user: '',
-        pass: '',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
       },
-    })
+    } as nodemailer.TransportOptions)
 
     const toEmail = 'vor.art.96@yandex.ru'
 
     await transporter.sendMail({
       from: '"Feedback Form" <v.artem.96@mail.ru>',
       to: toEmail,
-      subject: 'Новый отзыв с сайта',
-      text: `Оценка: ${rating}⭐\nКомментарий:\n${comment}`,
+      subject: 'Новый отзыв с сайта Food-paradise',
+      text: `Блюдо: ${recipe}\nОценка: ${rating}⭐\nКомментарий: ${comment}`,
     })
 
     return NextResponse.json({ success: true })
