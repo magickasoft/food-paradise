b947d5a2f64bb58f2cab10fab54cc34cd8e2eefe
Evgeny Shmakov <sjs-master@yandex.ru>
2025-10-18 18:52:52 +0700

    variables

---
+++ src/public_variables.ts
@@ -0,0 +1,5 @@
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+
+export const YANDEX_METRICA_ID = process.env.NEXT_PUBLIC_YANDEX_METRICA_ID
+
+export const URL = process.env.NEXT_PUBLIC_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
