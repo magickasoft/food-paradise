32b9587185725a97bdc891169620a65d056c8b93
Evgeny Shmakov <sjs-master@yandex.ru>
2025-10-18 19:11:06 +0700

    Update variables.ts

--- src/variables.ts
+++ src/variables.ts
@@ -2,12 +2,12 @@ export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 
 export const YANDEX_METRICA_ID = process.env.NEXT_PUBLIC_YANDEX_METRICA_ID
 
-export const URL = process.env.NEXT_PUBLIC_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL ?? 'food-paradise.ru'
+export const URL = process.env.NEXT_PUBLIC_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
 
-export const MAIL_HOST = process.env.NEXT_PUBLIC_MAIL_HOST
-export const MAIL_PORT = process.env.NEXT_PUBLIC_MAIL_PORT
-export const MAIL_USER = process.env.NEXT_PUBLIC_MAIL_USER
-export const MAIL_PASS = process.env.NEXT_PUBLIC_MAIL_PASS
+export const MAIL_HOST = process.env.MAIL_HOST
+export const MAIL_PORT = process.env.MAIL_PORT
+export const MAIL_USER = process.env.MAIL_USER
+export const MAIL_PASS = process.env.MAIL_PASS
 
 export const NODE_ENV = process.env.NODE_ENV
 export const isDevelopment = NODE_ENV === 'development'
