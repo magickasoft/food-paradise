c4fa7daf4d70ea403eaae848f67d06d8eecb6048
Evgeny Shmakov <sjs-master@yandex.ru>
2025-12-08 01:51:43 +0700

    Update page.tsx

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,4 +1,9 @@
 import { notFound } from 'next/navigation'
+import { locales } from '@/i18n/constants'
+
+export async function generateStaticParams() {
+  return locales.map(locale => ({ locale, rest: [] }))
+}
 
 export default function CatchAllPage() {
   notFound()
