bff999389fd2dd6cbbe3243e3b8fa72fbc0cd7c9
AtrVorobev <artvorobev96@gmail.com>
2026-01-23 16:34:04 +0300

    fix

--- src/components/support-project-btn/suggest-recipe-btn/index.tsx
+++ src/components/support-project-btn/suggest-recipe-btn/index.tsx
@@ -1,29 +1,12 @@
 'use client'
 
-import { memo, useCallback } from 'react'
-import { useTranslations } from 'next-intl'
-import { useMetrica } from '@artginzburg/next-ym'
-import { debounce } from '@/helpers/debounce'
+import { memo } from 'react'
 
 import { StyledButton } from './styles'
 
 export const SupportProjectButton = memo(function SupportProjectButton() {
-  // const t = useTranslations('suggestRecipe')
-  // const { reachGoal } = useMetrica()
-
-  // const handleClick = useCallback(
-  //   debounce(() => {
-  //     reachGoal?.('suggest_recipe')
-  //   }, 1000),
-  //   [reachGoal],
-  // )
-
-  // const title = t('title')
-
   return (
     <StyledButton
-      // onClick={handleClick}
-      // href={t('href')}
       title={'Поддержать проект'}
       aria-label={'Поддержать проект'}
       target="_blank"
