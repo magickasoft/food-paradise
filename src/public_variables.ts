9958500b55355f18913a47ec0a41875f5d193ad3
Evgeny Shmakov <sjs-master@yandex.ru>
2026-03-20 15:56:14 +0700

    metrika

--- src/public_variables.ts
+++ src/public_variables.ts
@@ -1,5 +1,3 @@
-export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+export const YANDEX_METRICA_ID = 103848917
 
-export const YANDEX_METRICA_ID = process.env.NEXT_PUBLIC_YANDEX_METRICA_ID
-
-export const URL = process.env.NEXT_PUBLIC_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
+export const URL = 'food-paradise.ru'
