d8c07364d8858635408a338c6b907e7ea064bade
AtrVorobev <artvorobev96@gmail.com>
2026-04-26 23:52:17 +0300

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,13 @@
 import type { MetadataRoute } from 'next'
-import type { Locale } from 'next-intl'
 import { paths } from '@/i18n/paths'
 import type { Href } from '@/i18n/createUrl'
 import { createEntity } from '@/i18n/createEntity'
+import { locales } from '@/i18n/constants'
 
 export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const values = Object.values(paths)
 
-  return values.flatMap(obj =>
-    Object.entries(obj)
-      .map(([locale, href]) => createEntity({ locale: locale as Locale, href: href as Href }))
-      .flat(),
-  )
+  return values.flatMap(obj => locales.flatMap(locale => createEntity({ locale, href: obj[locale] as Href })))
 }
