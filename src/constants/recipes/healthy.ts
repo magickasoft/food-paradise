3e4424621f4528e1cce063d1185cb48935ea240c
AtrVorobev <artvorobev96@gmail.com>
2026-05-12 10:20:32 +0300

    fix

--- src/constants/recipes/healthy.ts
+++ src/constants/recipes/healthy.ts
@@ -4,7 +4,16 @@ import { KINOA_BOUL_S_LOSOSEM } from './healthy/kinoa-boul-s-lososem'
 import { TYKVENNYJ_SUP_S_IMBIREM } from './healthy/tykvennyj-sup-s-imbirem'
 import { SMUZI_BOUL_S_CHIA } from './healthy/smuzi-boul-s-chia'
 import { FARSHIROVANNYE_PERTSY_S_KINOA } from './healthy/farshirovannye-pertsy-s-kinoa'
-import { NEW_HEALTHY_RECIPES } from './healthy/new-healthy'
+import { BATAT_S_KRASNOY_FASOLYU_I_AVOKADO } from './healthy/batat-s-krasnoy-fasolyu-i-avokado'
+import { TRESKA_S_BROKKOLI_I_LIMONOM } from './healthy/treska-s-brokkoli-i-limonom'
+import { TEPLYJ_SALAT_S_KINOA_TYKVOY_I_RUKKOLOY } from './healthy/teplyj-salat-s-kinoa-tykvoy-i-rukkoloy'
+import { TOFU_S_OVOSHCHAMI_V_KUNZHUTNOM_SOUSE } from './healthy/tofu-s-ovoshchami-v-kunzhutnom-souse'
+import { OVOSHCHNOY_SUP_S_FASOLYU_I_SHPINATOM } from './healthy/ovoshchnoy-sup-s-fasolyu-i-shpinatom'
+import { INDEYKA_S_KABACHKOM_I_ZELENYU } from './healthy/indeyka-s-kabachkom-i-zelenyu'
+import { SALAT_S_TUNTSOM_YAYTSOM_I_AVOKADO } from './healthy/salat-s-tuntsom-yaytsom-i-avokado'
+import { RIS_S_OVOSHCHAMI_I_KREVETKAMI } from './healthy/ris-s-ovoshchami-i-krevetkami'
+import { ZAPEKANKA_IZ_BROKKOLI_S_YAYTSOM } from './healthy/zapekanka-iz-brokkoli-s-yaytsom'
+import { SMUZI_S_CHERNIKOY_I_OVSYANKOY } from './healthy/smuzi-s-chernikoy-i-ovsyankoy'
 
 export const HEALTHY = [
   RAGU_IZ_KABACHKOV_V_SMETANE,
@@ -13,5 +22,14 @@ export const HEALTHY = [
   TYKVENNYJ_SUP_S_IMBIREM,
   SMUZI_BOUL_S_CHIA,
   FARSHIROVANNYE_PERTSY_S_KINOA,
-  ...NEW_HEALTHY_RECIPES,
+  BATAT_S_KRASNOY_FASOLYU_I_AVOKADO,
+  TRESKA_S_BROKKOLI_I_LIMONOM,
+  TEPLYJ_SALAT_S_KINOA_TYKVOY_I_RUKKOLOY,
+  TOFU_S_OVOSHCHAMI_V_KUNZHUTNOM_SOUSE,
+  OVOSHCHNOY_SUP_S_FASOLYU_I_SHPINATOM,
+  INDEYKA_S_KABACHKOM_I_ZELENYU,
+  SALAT_S_TUNTSOM_YAYTSOM_I_AVOKADO,
+  RIS_S_OVOSHCHAMI_I_KREVETKAMI,
+  ZAPEKANKA_IZ_BROKKOLI_S_YAYTSOM,
+  SMUZI_S_CHERNIKOY_I_OVSYANKOY,
 ]
