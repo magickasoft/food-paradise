f63a4a777922c571fafcc46d8ce24e2d79b4e5af
AtrVorobev <artvorobev96@gmail.com>
2026-05-22 17:34:09 +0300

    add new recipes

--- src/constants/recipes/refreshing-drinks.ts
+++ src/constants/recipes/refreshing-drinks.ts
@@ -11,6 +11,26 @@ import OGURECHNO_LAJMOVYJ_SPRITSER from './refreshing-drinks/ogurechno-lajmovyj-
 import MALINOVYJ_CHAJNYJ_FRESH from './refreshing-drinks/malinovyj-chajnyj-fresh.json'
 import PERSIKOVYJ_LATTE from './refreshing-drinks/persikovyj-latte.json'
 import INZHIRNYJ_KOKTEJL from './refreshing-drinks/inzhirnyj-koktejl.json'
+import LIMONAD_S_LAYMOM_I_BAZILIKOM from './refreshing-drinks/limonad-s-laymom-i-bazilikom.json'
+import OGURECHNO_MYATNAYA_VODA from './refreshing-drinks/ogurechno-myatnaya-voda.json'
+import KLUBNICHNYY_MORS_S_MYATOJ from './refreshing-drinks/klubnichnyy-mors-s-myatoj.json'
+import MALINA_LAYM_SPRITTSER from './refreshing-drinks/malina-laym-sprittser.json'
+import ARBUZNO_LAYMOVYY_FRESH from './refreshing-drinks/arbuzno-laymovyy-fresh.json'
+import MANGO_KOKOSOVYY_SMUZI from './refreshing-drinks/mango-kokosovyy-smuzi.json'
+import CHERNICHNYY_AYS_TI from './refreshing-drinks/chernichnyy-ays-ti.json'
+import ZELENYY_CHAY_S_LAYMOM_I_MYATOJ from './refreshing-drinks/zelenyy-chay-s-laymom-i-myatoj.json'
+import PERSIKOVYY_HOLODNYY_CHAY from './refreshing-drinks/persikovyy-holodnyy-chay.json'
+import LIMONAD_S_YAGODAMI_I_BAZILIKOM from './refreshing-drinks/limonad-s-yagodami-i-bazilikom.json'
+import KLYUKVENNYY_SPRITTSER_S_LAYMOM from './refreshing-drinks/klyukvennyy-sprittser-s-laymom.json'
+import IMBIRNO_LAYMOVYY_LIMONAD from './refreshing-drinks/imbirno-laymovyy-limonad.json'
+import OGURECHNO_BAZILIKOVYY_LIMONAD from './refreshing-drinks/ogurechno-bazilikovyy-limonad.json'
+import KLUBNICHNO_MYATNYY_SMUZI from './refreshing-drinks/klubnichno-myatnyy-smuzi.json'
+import ARBUZNO_OGURECHNYY_NAPITOK from './refreshing-drinks/arbuzno-ogurechnyy-napitok.json'
+import MALINOVYY_LIMONAD_S_BAZILIKOM from './refreshing-drinks/malinovyy-limonad-s-bazilikom.json'
+import YABLOCHNO_MYATNAYA_VODA from './refreshing-drinks/yablochno-myatnaya-voda.json'
+import KOKOSOVYY_NAPITOK_S_MANGO_I_CHIA from './refreshing-drinks/kokosovyy-napitok-s-mango-i-chia.json'
+import VINOGRADNO_LAYMOVYY_SPRITTSER from './refreshing-drinks/vinogradno-laymovyy-sprittser.json'
+import CHERNICHNO_LIMONNYY_SMUZI from './refreshing-drinks/chernichno-limonnyy-smuzi.json'
 
 export const REFRESHING_DRINKS: Recipe[] = [
   LIMONAD_S_MYATOJ,
@@ -25,4 +45,24 @@ export const REFRESHING_DRINKS: Recipe[] = [
   MALINOVYJ_CHAJNYJ_FRESH,
   PERSIKOVYJ_LATTE,
   INZHIRNYJ_KOKTEJL,
+  LIMONAD_S_LAYMOM_I_BAZILIKOM,
+  OGURECHNO_MYATNAYA_VODA,
+  KLUBNICHNYY_MORS_S_MYATOJ,
+  MALINA_LAYM_SPRITTSER,
+  ARBUZNO_LAYMOVYY_FRESH,
+  MANGO_KOKOSOVYY_SMUZI,
+  CHERNICHNYY_AYS_TI,
+  ZELENYY_CHAY_S_LAYMOM_I_MYATOJ,
+  PERSIKOVYY_HOLODNYY_CHAY,
+  LIMONAD_S_YAGODAMI_I_BAZILIKOM,
+  KLYUKVENNYY_SPRITTSER_S_LAYMOM,
+  IMBIRNO_LAYMOVYY_LIMONAD,
+  OGURECHNO_BAZILIKOVYY_LIMONAD,
+  KLUBNICHNO_MYATNYY_SMUZI,
+  ARBUZNO_OGURECHNYY_NAPITOK,
+  MALINOVYY_LIMONAD_S_BAZILIKOM,
+  YABLOCHNO_MYATNAYA_VODA,
+  KOKOSOVYY_NAPITOK_S_MANGO_I_CHIA,
+  VINOGRADNO_LAYMOVYY_SPRITTSER,
+  CHERNICHNO_LIMONNYY_SMUZI,
 ]
